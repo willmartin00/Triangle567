bba18129df5210cd2c57cc1eb8c6be7dfaad0e87
willmartin00 <wmartin@stevens.edu>
2022-02-09 20:06:11 -0500

    TestTriangle updated

--- TestTriangle.py
+++ TestTriangle.py
@@ -22,9 +22,30 @@ class TestTriangles(unittest.TestCase):
 
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+
+    def testRightTriangleC(self): 
+        self.assertEqual(classifyTriangle(6,8,10),'Right','6,8,10 is a Right triangle')
+
+    def testRightTriangleD(self): 
+        self.assertEqual(classifyTriangle(8,10,6),'Right','8,10,6 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTrianglesA(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testEquilateralTrianglesB(self): 
+        self.assertEqual(classifyTriangle(5,5,5),'Equilateral','5,5,5 should be equilateral')
+
+    def testIsoscelesTrianglesA(self):
+        self.assertEqual(classifyTriangle(4,4,2),'Isosceles','4,4,2 should be isosceles')
+
+    def testIsoscelesTrianglesB(self):
+        self.assertEqual(classifyTriangle(8,6,8),'Isosceles','8,6,8 should be isosceles')
+
+    def testScaleneTrianglesA(self):
+        self.assertEqual(classifyTriangle(6,7,8),'Scalene','6,7,8 should be scalene')
+
+    def testScaleneTrianglesB(self):
+        self.assertEqual(classifyTriangle(9,10,11),'Scalene','9,10,11 should be scalene')
 
 if __name__ == '__main__':
     print('Running unit tests')
